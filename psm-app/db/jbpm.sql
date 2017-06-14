51daf2fbf9a100904f0cbe6834547200a2ccbc04
Jason Owen <jason@jasonaowen.net>
2017-06-14 17:14:52 -0400

    Add jBPM required data for demo
    We are still relying on Hibernate to create the schema for now, but for
    the purpose of showing the progress we've made, add the data jBPM
    requires.

---
+++ psm-app/db/jbpm.sql
@@ -0,0 +1,7 @@
+-- Seed data *after* schema has been autocreated by Hibernate
+
+INSERT INTO organizationalentity (dtype, id) VALUES
+  ('Group', 'Service Agent'),
+  ('Group', 'Service Administrator'),
+  ('Group', 'System Administrator'),
+  ('User', 'Administrator');
